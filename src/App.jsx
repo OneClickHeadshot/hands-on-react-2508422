22a79c02dc18eaedaae5dd1460cec78702f7d7a1
Lajos Matyas Csepregi <44197233+OneClickHeadshot@users.noreply.github.com>
2023-06-15 09:06:18 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 function App() {
+  const name = 'WeirdAliens'
+  const style1 = {color:"steelblue"} 
+  //const style2 = {font-family:"verdana"}
+
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="group" />
+          <h1>Meet the <i style={style1}>{name}</i></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={()=> alert('Hey There!')}> Click Here for Surprise</button>
+        </hgroup>
+      </article>
+    </div>
   )
 }
 export default App
